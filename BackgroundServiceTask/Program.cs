81ae7845ad360be88fe9124352c232e33809bdfa
Oliver Vasilev <oliver.vasilev@damilah.com>
2024-12-10 10:10:10 +0100

    Add project files.

---
+++ BackgroundServiceTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        /// Register services here
+    })
+    .ConfigureLogging((context, logging) =>
+    {
+        logging.ClearProviders(); // Remove default providers
+        logging.AddConsole(); // Add Console logging provider
+    });
+
+var host = builder.Build();
+await host.RunAsync();
